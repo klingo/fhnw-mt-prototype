7a062863e385bc5d273bea04a59b8b6cdb49c646
klingo <klingo@klingonium.com>
2017-01-11 18:15:25 +0100

    start a draft implementation for icon selection

---
+++ Assets/CategoryIcon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategoryIcon : VRTK.VRTK_InteractableObject {
+
+    [Header("Prototype Options", order = 4)]
+
+    [Tooltip("The colour when the object is enabled")]
+    public Color enabledColor = Color.clear;
+
+    // Use this for initialization
+    protected void Start () {
+	}
+
+
+    public override void StartUsing(GameObject currentUsingObject) {
+        Debug.Log(currentUsingObject.name);
+        Debug.Log("hello");
+
+        GetComponent<Renderer>().material.color = enabledColor;
+
+        //base.StartUsing(currentUsingObject);
+    }
+
+    public override void StopUsing(GameObject previousUsingObject) {
+        //base.StopUsing(previousUsingObject);
+    }
+
+
+    // Update is called once per frame
+    protected override void Update () {
+        base.Update();
+	}
+}
