43af24d23cc9983985ba1ed09863a608922cb228
klingo <klingo@klingonium.com>
2017-01-11 22:15:23 +0100

    implement visual icon de-/activation

---
+++ Assets/Scripts/CategoryIcon.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using VRTK;
+
+public class CategoryIcon : VRTK_InteractableObject {
+
+    bool isActivated = false;
+    bool hasChanged = false;
+    Color defaultColor;
+
+    public override void StartUsing(GameObject usingObject) {
+        base.StartUsing(usingObject);
+
+        hasChanged = true;
+        isActivated = !isActivated;
+    }
+
+    public override void StopUsing(GameObject usingObject) {
+        base.StopUsing(usingObject);
+    }
+
+    protected void Start() {
+        defaultColor = gameObject.GetComponent<Renderer>().material.color;
+    }
+
+    protected override void Update() {
+        base.Update();
+
+        if (hasChanged) {
+            hasChanged = false;
+            if (isActivated) {
+                gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+            else {
+                gameObject.GetComponent<Renderer>().material.color = defaultColor; // Color.clear;
+            }
+        }
+    }
+}
