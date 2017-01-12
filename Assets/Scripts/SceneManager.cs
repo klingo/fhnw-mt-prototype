a6cccc1bb7d0b4ac852c59f1879b8c4156ab8fde
klingo <klingo@klingonium.com>
2017-01-12 22:44:59 +0100

    implement CSV to DataTable reader and add DataView with RowFilter

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,20 +1,78 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Data;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class SceneManager : Singleton<SceneManager> {
 
+    private const string CSV_REL_PATH = "\\Assets\\Resources\\CSV\\";
+    private const string CSV_FILE_NAME = "Expenses_2016_anonymized.csv";
+
+    // Singleton
     protected SceneManager() { }
 
+    // A hashset that contains all active categories
     public HashSet<string> activeCategories = new HashSet<string>();
 
     // Awake is always called before any Start functions
     void Awake() {
+        // Debug to inform that Singleton was created!
         Debug.Log("Awoke Singleton Instance: " + gameObject.GetInstanceID());
+    }
+
+
+    void Start() {
+        // Load CSV dataset
+        string csvFolderPath = Directory.GetCurrentDirectory() + CSV_REL_PATH;
+        string csvFilePath = csvFolderPath + CSV_FILE_NAME;
+
+        if (File.Exists(csvFilePath)) {
+            Debug.Log("FILE FOUND! :-)");
+
+            DataTable dataTable = ConvertCSVtoDataTable(csvFilePath);
+            DataView dataView = new DataView(dataTable);
+
+            Debug.Log("count 1: " + dataView.Count);
+
+            Debug.Log("test = " + dataTable.Rows[0].Table.Columns[6]);
+
+            dataView.RowFilter = "[Booking text] = 'INTERNET/PHONE'";
+            Debug.Log("count 2: " + dataView.Count);
+        }
+        else {
+            Debug.LogError(CSV_FILE_NAME + " could not be found!");
+        }
+
+
+
+        //DataView dv = new DataView(yourDatatable);
+        //dv.RowFilter = "query"; // query example = "id = 10"
     }
 
     // Update is called once per frame
     void Update() {
+    }
 
+
+    // Source: https://immortalcoder.blogspot.ch/2013/12/convert-csv-file-to-datatable-in-c.html
+    // 
+    private static DataTable ConvertCSVtoDataTable(string strFilePath) {
+        StreamReader sr = new StreamReader(strFilePath);
+        string[] headers = sr.ReadLine().Split(';');
+        DataTable dt = new DataTable();
+        foreach (string header in headers) {
+            dt.Columns.Add(header);
+        }
+        while (!sr.EndOfStream) {
+            string[] rows = Regex.Split(sr.ReadLine(), ";(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+            DataRow dr = dt.NewRow();
+            for (int i = 0; i < headers.Length; i++) {
+                dr[i] = rows[i];
+            }
+            dt.Rows.Add(dr);
+        }
+        return dt;
     }
 }
---
+++ Assets/Scripts/datamodel/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Transaction {
+
+    public DateTime date { get; private set; }
+    public String recipient { get; private set; }
+    public String accountName { get; private set; }
+    public String accountNo { get; private set; }
+    public String currency { get; private set; }
+    public float amount { get; private set; }
+    public String bookingtext { get; private set; }
+    public String category { get; private set; }
+
+    // Constructor
+    public Transaction(DateTime aDate, String aRecipient, String aAccountName, String aAccountNo, String aCurrency, float aAmount, String aBookingtext, String aCategory) {
+        this.date = aDate;
+        this.recipient = aRecipient;
+        this.accountName = aAccountName;
+        this.accountNo = aAccountNo;
+        this.currency = aCurrency;
+        this.amount = aAmount;
+        this.bookingtext = aBookingtext;
+        this.category = aCategory;
+    }
+
+}
