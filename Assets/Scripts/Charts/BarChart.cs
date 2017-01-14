d67e0003b84ac582d0014ac1007d990ec521f14c
klingo <klingo@klingonium.com>
2017-01-14 16:02:53 +0100

    fix some more label alignments and prepare raycasting

--- Assets/Scripts/Charts/BarChart.cs
+++ Assets/Scripts/Charts/BarChart.cs
@@ -45,7 +45,7 @@ public class BarChart : MonoBehaviour {
         float maxValue = vals.Max();
         float normalizedThresholdValue = threshold / maxValue;
 
-        for (int i = 0; i < vals.Length; i++) {
+        for (int currBarIndex = 0; currBarIndex < vals.Length; currBarIndex++) {
             // Instantiate new Bar
             Bar newBarHolder = Instantiate(barHolderPrefab) as Bar;
 
@@ -58,7 +58,7 @@ public class BarChart : MonoBehaviour {
             // Set the size of the bars
             RectTransform rtBottomBar = newBarHolder.bottomBar.GetComponent<RectTransform>();
             RectTransform rtTopBar = newBarHolder.topBar.GetComponent<RectTransform>();
-            float bottomVal = vals[i];
+            float bottomVal = vals[currBarIndex];
             float topVal = 0f;
             if (bottomVal > threshold) {
                 topVal = bottomVal - threshold;
@@ -70,27 +70,51 @@ public class BarChart : MonoBehaviour {
             rtBottomBar.sizeDelta = new Vector3(rtBottomBar.sizeDelta.x, chartHeight * normalizedBottomValue);
             rtTopBar.sizeDelta = new Vector3(rtTopBar.sizeDelta.x, chartHeight * normalizedTopValue);
 
+            // Set the special paddings
+            VerticalLayoutGroup vlg = newBarHolder.GetComponent<VerticalLayoutGroup>();
+            if (currBarIndex > 0) {
+                // all bars except the first one
+                vlg.padding.left = 5;
+            } else if (currBarIndex < (vals.Length - 1)) {
+                // all bars except the last one
+                vlg.padding.right = 5;
+            }
+            
+
             // Set the color of the bars
             newBarHolder.bottomBar.color = bottomBarColor;
             newBarHolder.topBar.color = topBarColor;
 
             // Set the bottom label of the bar
-            if (labels.Length <= i) {
+            if (labels.Length <= currBarIndex) {
                 newBarHolder.label.text = "UNDEFINED";
             } else {
-                newBarHolder.label.text = labels[i];
+                newBarHolder.label.text = labels[currBarIndex];
+            }
+
+            // due to the special paddings, the label position for the bars has to be adjustet: first +7.5f, all others -7.5f
+            Vector3 labelLocalPos = newBarHolder.label.rectTransform.localPosition;
+            if (currBarIndex > 0) {
+                newBarHolder.label.rectTransform.localPosition = new Vector3(-7.5f, labelLocalPos.y);
+            } else {
+                newBarHolder.label.rectTransform.localPosition = new Vector3(7.5f, labelLocalPos.y);
             }
 
             // Set the value label at the top of the bar
             if (topVal > 0f) {
                 // put the label at the top bar
-                newBarHolder.topBarValue.text = vals[i].ToString();
+                newBarHolder.topBarValue.text = vals[currBarIndex].ToString();
                 newBarHolder.bottomBarValue.text = string.Empty;
+                // if height is too small, move label to top of bar
+                if (rtTopBar.sizeDelta.y < 30f) {
+                    newBarHolder.topBarValue.rectTransform.pivot = new Vector2(0.5f, 0f);
+                    newBarHolder.topBarValue.rectTransform.anchoredPosition = Vector2.zero;
+                }
             } else {
                 // put the label at the bottom bar
                 newBarHolder.topBarValue.text = string.Empty;
-                newBarHolder.bottomBarValue.text = vals[i].ToString();
-                // if height is too small, move labelt ot op of bar
+                newBarHolder.bottomBarValue.text = vals[currBarIndex].ToString();
+                // if height is too small, move label to top of bar
                 if (rtBottomBar.sizeDelta.y < 30f) {
                     newBarHolder.bottomBarValue.rectTransform.pivot = new Vector2(0.5f, 0f);
                     newBarHolder.bottomBarValue.rectTransform.anchoredPosition = Vector2.zero;
