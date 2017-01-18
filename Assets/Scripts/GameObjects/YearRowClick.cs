cf2e83331733be95f53a1f06ca7ab7a6f611d420
klingo <klingo@klingonium.com>
2017-01-18 02:17:15 +0100

    draft preparation of year selection canvas

---
+++ Assets/Scripts/GameObjects/YearRowClick.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class YearRowClick : VRTK_InteractableObject {
+
+    public override void StartUsing(GameObject currentUsingObject) {
+        base.StartUsing(currentUsingObject);
+    }
+
+    public override void StartTouching(GameObject currentTouchingObject) {
+        base.StartTouching(currentTouchingObject);
+    }
+
+    public override void StopTouching(GameObject previousTouchingObject) {
+        base.StopTouching(previousTouchingObject);
+    }
+}
---
+++ Assets/Scripts/Tables/Year.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Year : MonoBehaviour {
+
+    [Header("[Image]", order = 0)]
+    public Image yearImage;
+
+    [Header("[Text]", order = 1)]
+    public Text yearText;
+}
