a725b0375d6ef5f024f431192a39c921b2e00625
klingo <klingo@klingonium.com>
2017-01-23 11:34:30 +0100

    add MIT license to all necesary locations, move Singleton class to Utilities, delete empty Shaders folder

--- Assets/Scripts/Utilitites/Singleton.cs
+++ Assets/Scripts/Utilitites/Singleton.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 
-// Based on http://wiki.unity3d.com/index.php/Singleton
-
 /// <summary>
 /// Be aware this will not prevent a non singleton constructor
 ///   such as `T myT = new T();`
 /// To prevent that, add `protected T () {}` to your singleton class.
 /// 
 /// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// 
+/// Based on http://wiki.unity3d.com/index.php/Singleton
 /// </summary>
 public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
     private static T _instance;
